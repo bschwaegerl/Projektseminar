237d57a412d8ef00d31e1ff9a5239d96b2ac4b70
sltzr <benedikt.schwaegerl@stud.uni-regensburg.de>
2016-11-17 17:27:18 +0100

    Searchfunction added
    searching in db available! pääm

--- InnovationTool/index.php
+++ InnovationTool/index.php
@@ -3,11 +3,31 @@
 
 <body>
 	<?php
+		$searchedWord = "Auto";
+		$tableName = get_table_name($searchedWord);
+		
+		
 		$hostname = "localhost"; $user = "root"; $password = ""; $db = "inno_blacklist";
+		
 		$connection = mysqli_connect($hostname, $user, $password, $db);
-			if ($result = mysqli_query($connection, "SELECT * FROM a")){ while ($row = mysqli_fetch_array($result)){
-				echo utf8_encode(("ID: ".$row[0].", Name: ".$row[1]."<br />")); }
-		mysqli_free_result($result); }
-	mysqli_close($connection); ?>
+			if ($result = mysqli_query($connection, "SELECT * FROM $tableName where word = '".$searchedWord."' ")){ 
+				while ($row = mysqli_fetch_array($result)){
+					echo utf8_encode(("ID: ".$row[0].", Name: ".$row[1]."<br />"));
+					}
+			mysqli_free_result($result); 
+			}
+			else {
+				echo "0 results";
+			}
+	mysqli_close($connection); 
+	
+	
+	function get_table_name($word){
+		
+		return substr($word,0,1);
+		
+	}
+	
+	?>
 </body> 
 </html>
