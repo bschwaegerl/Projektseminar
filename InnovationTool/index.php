166383995d74b8cf5e6ad44d6b1459c5dc323f15
sltzr <benedikt.schwaegerl@stud.uni-regensburg.de>
2016-11-21 16:11:28 +0100

    Datenbankabgleich mit Wikipedia
    ä,ö,ü Probleme sowie "US" "RSS Feed" etc. ist noch problematisch

--- InnovationTool/index.php
+++ InnovationTool/index.php
@@ -3,28 +3,68 @@
 
 <body>
 	<?php
-		$searchedWord = "Auto";
+		include_once('simple_html_dom.php');
+		set_time_limit(0);
+		
+		$url = $_POST['websitelink'];
+		
+		//$text befüllen und Sonderzeichen durch Leerzeichen ersetzen
+		$text = preg_replace('/[^\p{Latin}\s]/u', ' ', (file_get_html($url)->plaintext));
+		//Mehrfachleerzeichen zu einem umwandeln
+		$text = preg_replace('!\s+!', ' ', $text);
+		//Leerzeichen einfügen
+		$text = preg_replace('/(?<!\ )[A-Z]/', ' $0', $text);
+		
+		//String in Array umwandeln
+		$wordsOfWebsite = explode(" ",$text);
+		//Duplikate entfernen
+		$wordsOfWebsite = array_unique(array_values(array_filter($wordsOfWebsite)));
+		$wordsOfWebsite[] = 'schwägerl';
+		
+		//Datenbankverbindung aufbauen
+		$hostname = "localhost"; $user = "root"; $password = ""; $db = "inno_blacklist";
+		$connection = mysqli_connect($hostname, $user, $password, $db);
+		
+		foreach($wordsOfWebsite as $key=>$searchedWord){
+			
 		$tableName = get_table_name($searchedWord);
 		
-		
-		$hostname = "localhost"; $user = "root"; $password = ""; $db = "inno_blacklist";
-		
-		$connection = mysqli_connect($hostname, $user, $password, $db);
-			if ($result = mysqli_query($connection, "SELECT * FROM $tableName where word = '".$searchedWord."' ")){ 
-				while ($row = mysqli_fetch_array($result)){
-					echo utf8_encode(("ID: ".$row[0].", Name: ".$row[1]."<br />"));
+			if($result = mysqli_query($connection, "SELECT word FROM $tableName where word = '".$searchedWord."' " )){
+			
+			
+				while($row = mysqli_fetch_array($result)){
+				
+					if (strcmp(strtolower($searchedWord),strtolower($row[0]))== 0){ 
+					
+					unset($wordsOfWebsite[$key]);
 					}
-			mysqli_free_result($result); 
-			}
-			else {
-				echo "0 results";
-			}
-	mysqli_close($connection); 
+				}
+					mysqli_free_result($result); 
+			}	
+			
+			
+		}
+		print_r(array_values($wordsOfWebsite));
+		
+		mysqli_close($connection); 
 	
 	
 	function get_table_name($word){
-		
-		return substr($word,0,1);
+		$firstLetter = substr($word,0,1);
+		switch($firstLetter){
+			case 'ü': 
+				$firstLetter = 'u';
+				break;
+			case 'ä': 
+				$firstLetter = 'a';
+				break;
+			case 'ö': 
+				$firstLetter = 'o';
+				break;
+			default:
+				break;
+		}
+		return $firstLetter;
 		
 	}
 	
