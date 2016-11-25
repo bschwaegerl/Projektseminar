0ce68c5711e8382b4ec67f034a7b29e3d9437030
philipempl <philip.empl@gmx.de>
2016-11-25 12:44:20 +0100

    index.php

--- InnovationTool/index.php
+++ InnovationTool/index.php
@@ -12,6 +12,7 @@
 		$text = file_get_html($url)->plaintext;
 				
 		//Sonderzeichen umwandeln
+		$text = preg_replace('~&nbsp;~', ' ', $text);
 		$text = preg_replace('~&auml;~', 'ä', $text);
 		$text = preg_replace('~&ouml;~', 'ö', $text);
 		$text = preg_replace('~&uuml;~', 'ü', $text);
@@ -20,14 +21,12 @@
 		$text = preg_replace('~&Ouml;~', 'Ö', $text);
 		$text = preg_replace('~&Uuml;~', 'Ü', $text);
 		
-		$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $text);
-fclose($myfile);
+		// $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+// fwrite($myfile, $text);
+// fclose($myfile);
 
 		 //$text befüllen und Sonderzeichen durch Leerzeichen ersetzen
 		$text = preg_replace('/[^\p{Latin}\s]/u', ' ', $text);
-		//Leerzeichen einfügen
-		$text = preg_replace('/(?<!\ )[A-Z]/', ' $0', $text); 
 		//Mehrfachleerzeichen zu einem umwandeln
 		$text = preg_replace('!\s+!', ' ', $text);
 		
@@ -36,20 +35,22 @@ fclose($myfile);
 		 $wordsOfWebsite = explode(" ",$text);
 		//Duplikate entfernen
 		$wordsOfWebsite = array_unique(array_values(array_filter($wordsOfWebsite))); 
+		$wordsOfWebsite=array_map('trim',$wordsOfWebsite);
+
 		
 		echo "Wörter insgesamt: " . count($wordsOfWebsite) . '<br>'; 
 		
 		//Datenbankverbindung aufbauen
-		$hostname = "localhost"; $user = "root"; $password = ""; $db = "inno_blacklist";
+		$hostname = "localhost"; $user = "root"; $password = ""; $db = "innovation";
 		$connection = mysqli_connect($hostname, $user, $password, $db);
 		
 		foreach($wordsOfWebsite as $key=>$searchedWord){
 					
-		$tableName = get_table_name($searchedWord);
+		$tableName = utf8_decode(get_table_name($searchedWord));
 		
 			$searchedWord = utf8_decode($searchedWord);
 			
-			if($result = mysqli_query($connection, "SELECT word FROM $tableName where word = '".$searchedWord."' " )){
+			if($result = mysqli_query($connection, "SELECT word FROM $tableName where MATCH(word) AGAINST('".$searchedWord."') " )){
 			
 
 				while($row = mysqli_fetch_array($result)){
@@ -64,6 +65,7 @@ fclose($myfile);
 			
 			
 		}
+		
 		
 		mysqli_close($connection); 
 		
@@ -83,19 +85,6 @@ fclose($myfile);
 		$firstLetter = mb_substr($word,0,1,"UTF-8");
 		$firstLetter = mb_strtolower($firstLetter, "UTF-8");
 		
-		switch($firstLetter){
-			case 'ü': 
-				$firstLetter = 'u';
-				break;
-			case 'ä': 
-				$firstLetter = 'a';
-				break;
-			case 'ö': 
-				$firstLetter = 'o';
-				break;
-			default:
-				break;
-		}
 		return $firstLetter;
 		
 	}
