4b9d0e5c467cb5a3308dddac4d50ac966cb5562a
philipempl <philip.empl@gmx.de>
2016-12-09 13:48:17 +0100

    Fertisch mit hinfzufügen

--- InnovationTool/showInnovations.php
+++ InnovationTool/showInnovations.php
@@ -4,15 +4,46 @@
 		<?php
 		
 			set_time_limit(0);
+			date_default_timezone_set('Europe/Berlin');
+
 			
 			//database connection
 			$connection = buildDatabaseConnection();
 			
+			//if delete clicked
 			if(isset($_POST['löschen'])){
-			$updateInnoFoundUrls = "DELETE FROM _innovation_found_urls WHERE id = $_POST[hidden]";
+				
+			$deleteInnoFoundUrls = "DELETE FROM _innovation_found_urls WHERE id = $_POST[hiddenFoundUrl]";
+			mysqli_query($connection, $deleteInnoFoundUrls);
 
-			mysqli_query($connection, $updateInnoFoundUrls);
-			}			
+			$checkQuantity = "SELECT * FROM _innovation_found_urls WHERE id = $_POST[hiddenFoundUrl]" ;
+			if(mysqli_num_rows(mysqli_query($connection, $checkQuantity)) == 0)
+			{
+				mysqli_query($connection, "DELETE FROM _innovation_found WHERE id = $_POST[hiddenFound]");
+			}	
+			
+			}
+
+			//if add clicked
+			if(isset($_POST['hinzufügen'])){
+				
+			$insertInnoFound = "INSERT INTO _innovation_found (word) 
+				SELECT * FROM (SELECT '$_POST[wort]') as tmp WHERE NOT EXISTS
+			(SELECT * FROM _innovation_found where word = '$_POST[wort]') LIMIT 1";
+			mysqli_query($connection, $insertInnoFound);
+			
+			$insertInnoFoundUrls = "INSERT INTO _innovation_found_urls (innovation_found_id, url, date) 
+					SELECT * FROM (SELECT
+				(SELECT id from _innovation_found where word = '$_POST[wort]'),
+				'$_POST[url]',
+				'".date("Y-m-d H:i:s", time())."') as tmp WHERE NOT EXISTS
+				(SELECT * from _innovation_found_urls where url = '$_POST[url]'				
+				AND innovation_found_id =
+				(SELECT id from _innovation_found where word = '$_POST[wort]'))LIMIT 1"; 
+			
+			mysqli_query($connection, $insertInnoFoundUrls);
+
+			}				
 			//get table innovation_found
 			$innovationFoundTable = getInnoFoundTable($connection);
 			
@@ -75,7 +106,7 @@
 			foreach($innovationFoundTable as $innovationFound){
 			
 				foreach($innovationFoundUrlsTable as $innovationFoundUrls){
-					
+						
 					//if foreign key equals primary key than draw record
 					if($innovationFound[0] == $innovationFoundUrls[1]){
 						
@@ -84,14 +115,22 @@
 						echo "<td>" . $innovationFound[1]. "</td>";
 						echo "<td> <a href="  . $innovationFoundUrls[2] . "</a>"   . $innovationFoundUrls[2] . "</td>";
 						echo "<td>"  . $innovationFoundUrls[3]. "</td>";
-						echo "<td> <input type=hidden name=hidden value=" . $innovationFoundUrls[0]. "></td>";
-						echo "<td> <input type=submit name=löschen value=löschen></td>";
+						echo "<td> <input type=submit name=löschen value=-></td>";
 						echo "</tr>";
+						echo "<input type=hidden name=hiddenFoundUrl value=" . $innovationFoundUrls[0]. ">";
+						echo "<input type=hidden name=hiddenFound value=" . $innovationFound[0]. ">";
 						echo "</form>";
 					}
 				}	
 			}
-			
+			echo "<form acion=showInnovations.php method=post>";
+			echo "<tr>";
+			echo "<td><input type=text name=wort value=> </td>";
+			echo "<td> <input type=text name=url value=>  </td>";
+			echo "<td>" .date('d/m/Y H:i:s', time()). "</td>";
+			echo "<td> <input type=submit name=hinzufügen value=+></td>";
+			echo "</tr>";
+			echo "</form>";
 			echo "</table>";
 			
 			}
